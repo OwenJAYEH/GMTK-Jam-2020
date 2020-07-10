8819fea08d20f84746a4bda80eb15f177f7ce44f
OwenJAYEH <OwenJAYEH@gmail.com>
2020-07-10 13:52:15 -0700

    Player Movement

--- GMTK-Jam-2020/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ GMTK-Jam-2020/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,39 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Initialized public variables for use inside of the inspector
+        public CharacterController2D controller;
 
-    public CharacterController2D controller;
-
-    public float runSpeed = 40f;
-
-    float horizontalMove = 0f;
-
+    // Initialized variables
+        // Variable for player run speed
+        public float runSpeed = 100f;
+        // Variable used for player movement left and right
+        float horizontalMove = 0f;
+    
+    // Booleans for Jumping
     bool jump = false;
-    bool crouch = false;
+
+    public bool tripleSpeed = false;
 
     // Update is called once per frame
     void Update()
     {
+        switch (tripleSpeed)
+        {
+            case false:
+                runSpeed = 100f;
+                break;
+            case true:
+                runSpeed = 300f;
+                break;
+        }
+        // Uses players input of A and D keys to move character in FixedUpdate
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        // Uses players input of SpaceBar to make the character Jump
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
-        }
-
-        if (Input.GetButtonDown("Crouch"))
-        {
-            crouch = true;
-        } else if (Input.GetButtonUp("Crouch"))
-        {
-            crouch = false;
         }
     }
 
     void FixedUpdate()
     {
         // Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        // Disables jump so player cannot jump twice
         jump = false;
     }
 }
