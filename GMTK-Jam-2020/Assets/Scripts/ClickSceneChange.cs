29f82f28eb4b2e39c89fdeb2f35cef7306d39232
Max Cannon <66145231+MaxCannonWF@users.noreply.github.com>
2020-07-10 21:11:16 -0600

    Added splash screen and build settings

---
+++ GMTK-Jam-2020/Assets/Scripts/ClickSceneChange.cs
@@ -0,0 +1,23 @@
+﻿ using System.Collections;
+ using UnityEngine;
+ using UnityEngine.SceneManagement;
+ 
+ public class ClickSceneChange : MonoBehaviour
+ {
+     public float delay = 440;
+     public string NewLevel= "TitleScreen";
+     void Start()
+     {
+         StartCoroutine(LoadLevelAfterDelay(delay));
+     }
+ 
+     IEnumerator LoadLevelAfterDelay(float delay)
+     {
+         yield return new WaitForSeconds(delay);
+         SceneManager.LoadScene(NewLevel);
+    }
+    void Update()
+    {
+        if(Input.anyKey) SceneManager.LoadScene(NewLevel);
+    }
+}
