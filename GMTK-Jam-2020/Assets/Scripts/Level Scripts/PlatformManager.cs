1d1611c8fe54fe68384b4b12e01f0871d7b02352
OwenJAYEH <OwenJAYEH@gmail.com>
2020-07-10 19:01:55 -0700

    SlowMotion and Project Fixed

--- GMTK-Jam-2020/Assets/Scripts/Level Scripts/PlatformManager.cs
+++ GMTK-Jam-2020/Assets/Scripts/Level Scripts/PlatformManager.cs
@@ -9,11 +9,10 @@ public class PlatformManager : MonoBehaviour
     GameObject currentPlatform;
     int index;
 
+    public bool DropEvent = false;
+
     // GameObject for the collision of currently selected platform
     public GameObject currentPlatCol;
-
-    public bool DropEvent = false;
-    public PlatformDrop PlatformDropScript;
 
 
     // Start is called before the first frame update
@@ -23,8 +22,7 @@ public class PlatformManager : MonoBehaviour
         index = Random.Range(0, platforms.Length); // randomly selects one platform
         currentPlatform = platforms[index]; // registers random platform as the one the player must get to
         print(currentPlatform.name); // Prints name for debug purposes
-        currentPlatCol.transform.position = new Vector2 (currentPlatform.transform.position.x, currentPlatform.transform.position.y + .45f); // Moves collission to platform and adds height
-                                                                                                                                             // to make it only reachable from the top
+        currentPlatCol.transform.position = new Vector2 (currentPlatform.transform.position.x, currentPlatform.transform.position.y + .45f); // Moves collission to platform and adds height                                                                                                                                    // to make it only reachable from the top
     }
 
     // Function repeats the process above
@@ -37,9 +35,36 @@ public class PlatformManager : MonoBehaviour
 
     void Update()
     {
-        if (DropEvent == true)
+        switch (DropEvent)
         {
-            PlatformDropScript.DropEventToggle();
+            case false:
+                DropEventToggleOff();
+                break;
+            case true:
+                DropEventToggleOn();
+                break;
         }
     }
+
+    void DropEventToggleOn()
+    {
+        platforms = GameObject.FindGameObjectsWithTag("platform"); // Creates an array of all objects with the tag platform
+        for (int i = 0; i < platforms.Length; i++)
+        {
+            platforms[i].GetComponent<PlatformDrop>().enabled = true;
+        }
+    }
+
+    void DropEventToggleOff()
+    {
+        platforms = GameObject.FindGameObjectsWithTag("platform"); // Creates an array of all objects with the tag platform
+        for (int i = 0; i < platforms.Length; i++)
+        {
+            platforms[i].GetComponent<PlatformDrop>().StartOver();
+            platforms[i].GetComponent<Renderer>().enabled = true;
+            platforms[i].GetComponent<BoxCollider2D>().enabled = true;
+            platforms[i].GetComponent<PlatformDrop>().enabled = false;
+        }
+    }
+
 }
