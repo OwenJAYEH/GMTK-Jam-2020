3baf0ac5149b7ddfb0597e85aef6de5640f896f8
OwenJAYEH <OwenJAYEH@gmail.com>
2020-07-10 19:50:34 -0700

    Camera Flip

---
+++ GMTK-Jam-2020/Assets/CameraFlip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlip : MonoBehaviour
+{
+
+    public bool flipCamera = false;
+    void Update()
+    {
+
+        switch (flipCamera)
+        {
+            case false:
+                transform.eulerAngles = new Vector3(0, 0, 0f);
+                break;
+            case true:
+                transform.eulerAngles = new Vector3(0, 0, 180f);
+                break;
+        }
+    }
+}
